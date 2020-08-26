adf21f10cddee7db9deb6d3293fe7de1710dd5ab
VicsStuff <61487136+VicsStuff@users.noreply.github.com>
2020-08-26 19:13:19 +1200

    Implemented tile and token viewers as JPanels

--- 0 SWEN225 assi1/view/TileView.java
+++ 0 SWEN225 assi1/view/TileView.java
@@ -1,5 +1,30 @@
 package view;
 
-public class TileView {
+import javax.swing.*;
+import java.awt.*;
 
+public class TileView extends JPanel {
+    //attributes
+    private JLabel label;
+    private ImageIcon tilePicture;
+    private String tileToolTipText;
+    private String tokenToolTipText = "";
+
+    public TileView(Image imageFile, String text) {
+        super();
+        //set image
+        tilePicture = new ImageIcon(imageFile);
+        label = new JLabel();
+        label.setIcon(tilePicture);
+        //set tool tip
+        tileToolTipText = text;
+        label.setToolTipText(tileToolTipText+tokenToolTipText);
+
+        add(label);  //add JLabel for image
+    }
+
+    public void setTokenToolTipText(String tokenToolTipText) {
+        this.tokenToolTipText = tokenToolTipText;
+        label.setToolTipText(tileToolTipText+tokenToolTipText);
+    }
 }
